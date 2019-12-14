478d95c19e4090e8b6312d83aa3516efd514051c
AnasTrikrisna <44199864+AnasTrikrisna@users.noreply.github.com>
2019-12-14 23:25:55 +0700

    update data riwayat pendidikan

--- application/views/templates/user/dataTableRiwayatPendidikan.php
+++ application/views/templates/user/dataTableRiwayatPendidikan.php
@@ -7,7 +7,7 @@
                     </i>
                 </div>
                 <div>Data <?= $main['menu']; ?>
-                    <div class="page-title-subheading">Hobiku Kebiasaanku
+                    <div class="page-title-subheading">Pendidikanku Prestasiku 
                     </div>
                 </div>
             </div>
@@ -22,8 +22,8 @@
                     <div class="card mb-3 widget-content bg-midnight-bloom">
                         <div class="widget-content-wrapper text-white">
                             <div class="widget-content-left">
-                                <div class="widget-heading">Tambah Hobi</div>
-                                <div class="widget-subheading">Hobiku</div>
+                                <div class="widget-heading">Tambah Pendidikan</div>
+                                <div class="widget-subheading">Pendidikan</div>
                             </div>
                             <div class="widget-content-right">
                                 <div class="widget-numbers text-white">
@@ -44,8 +44,10 @@
                                 <tr role="row">
                                     <?php
                                     $nama_column = array(
-                                        'No',
-                                        'Hobi',
+                                        'id_pendidikan',
+                                        'ket_pendidikan',
+                                        'nama_sekolah',
+                                        'tahun_lulus',
                                         'action'
                                     );
                                     foreach ($nama_column as $value) : ?>
@@ -58,7 +60,9 @@
                                 foreach ($table as $key => $row) : ?>
                                     <tr role="row" class="odd">
                                         <td tabindex="0" class="sorting_1"><?= $i ?></td>
-                                        <td><?= $row->ket_hobi ?></td>
+                                        <td><?= $row->ket_pendidikan ?></td>
+                                        <td><?= $row->nama_sekolah ?></td>
+                                        <td><?= $row->tahun_lulus ?></td>
                                         <td class="text-center">
                                             <!-- <a class="btn btn-primary text-white"><i class="fas fa-eye"></i></a> -->
                                             <a href="<?= base_url('user/hobi/edit/') . $row->id ?>" class="btn btn-success" type="button"><i class="fas fa-edit"></i></a>
